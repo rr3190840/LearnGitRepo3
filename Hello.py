43d422e8e8defcd949166048450653ff88e7c5d8
Ravi Ranjan <rr3190840@gmail.com>
2020-08-30 18:38:08 +0530

    Hello.py

--- Hello.py
+++ Hello.py
@@ -3,3 +3,5 @@ print("Let's talk sth more about loop!")
 
 print("Python is one of the object oriented programming
 	language")
+print("In dev branch!")
+print("Hi, there we are using sublime text!")
--- forloops.py
+++ forloops.py
@@ -2,3 +2,5 @@ for i in range(10):
 	print("MindBogling!")
 
 print("What about while loop")
+
+print("Be true to youself")
